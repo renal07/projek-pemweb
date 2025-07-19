6e8f1b532ec4dcd5a0b8cbfb3fd0cd4fedf24c71
rajwa123321 <rajwaaanggara123@gmail.com>
2025-07-19 14:34:57 +0700

    hapus 1

--- back-end/api_2/index.php
+++ back-end/api_2/index.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <title>API Kedai Ramen</title>
-    <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
